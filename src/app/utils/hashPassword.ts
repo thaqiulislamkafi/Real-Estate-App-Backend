4a660eeb06125ded547c2a495d37bd5331ed631a
Thaqi Ul Islam Kafi <thaqiulislamkafi@tuta.io>
2026-04-25 19:34:38 +0600

    Author related info updated

--- src/app/utils/hashPassword.ts
+++ src/app/utils/hashPassword.ts
@@ -1,5 +1,11 @@
 import bcrypt from 'bcrypt'
 
+/**
+ * @author Thaqi Ul Islam Kafi
+ * @description This file contains a utility function for hashing passwords using bcrypt.
+ * @generated 2026-04-25
+ */
+
 export const hashPassword = async (password:string) :Promise<string> =>{
 
     const saltRounds = 10 ;
